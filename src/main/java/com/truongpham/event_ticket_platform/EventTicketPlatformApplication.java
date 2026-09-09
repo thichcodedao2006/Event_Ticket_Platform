dc534eead6b331631e7592eb6d4200dafc618d35
thichcodedao2006 <truongpd.a2.2124@gmail.com>
2026-09-09 16:36:08 +0700

    Init Project

---
+++ src/main/java/com/truongpham/event_ticket_platform/EventTicketPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.truongpham.event_ticket_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTicketPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventTicketPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/truongpham/event_ticket_platform/EventTicketPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truongpham.event_ticket_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTicketPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
